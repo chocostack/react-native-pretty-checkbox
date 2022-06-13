52cba4014ab2a53542c7ed5203e044e9627cee3c
DESKTOP-U7TEIJ5\Salim <salim.assad.sanchez@gmail.com>
2022-06-13 18:11:08 -0500

    no message

---
+++ PrettyCheckbox.js
@@ -0,0 +1,38 @@
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import React from 'react';
+import { View } from 'react-native';
+
+const PrettyCheckbox = (props) => {
+    let color = '#999';
+    let backgroundColor = 'white';
+
+    if (props.selected) {
+        color = props.selectedColor ? props.selectedColor : color;
+        backgroundColor = props.selectedColor ? props.selectedColor : backgroundColor;
+    }
+
+    return (
+        <View style={[{
+            height: 24,
+            width: 24,
+            borderRadius: 12,
+            borderWidth: 2,
+            borderColor: color,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: backgroundColor
+        }, props.style]}>
+            {
+                props.selected ?
+                    <FontAwesomeIcon
+                        icon={faCheck}
+                        color={'white'}
+                    />
+                    : null
+            }
+        </View>
+    );
+};
+
+export default PrettyCheckbox;
